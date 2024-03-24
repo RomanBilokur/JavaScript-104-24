404fac92f4dc095c7f67c76ef6a54c516e89bb83
RomanBilokur <80679043206@ukr.net>
2024-03-25 01:10:39 +0200

    new

---
+++ script.js
@@ -0,0 +1,80 @@
+// const username = "Poly";
+// const message = "Welcome " + username;
+// console.log (message);
+
+
+// const age = 24;
+// const messagee = "Poly is " + age + " years old!";
+// console.log (messagee)
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const messagem = `You picked ${productName}, price per item is ${pricePerItem} credits`
+// Change code below this line
+// console.log(messagem);
+
+
+
+// const age = 16;
+// const isAdult = 18;
+// console.log(age<isAdult);
+
+// let age = 20;
+// let isAdult = age >= 18;
+// console.log(isAdult);
+ 
+// console.log(5==5);
+// console.log(5==3);
+// console.log(5!=3);
+// console.log(5!=5);
+// c
+// console.log(typeof typeof 1);
+
+console.log("A");
+
+function logStuff() {
+	console.log("B");
+}
+
+console.log("C");
+
+logStuff();
+
+console.log("D");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
